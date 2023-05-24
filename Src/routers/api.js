05d38ee3542763e4b49bb5b8c4029d3daaa430e2
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2023-05-24 16:38:36 -0300

    “Base-da-API”

--- Src/routers/api.js
+++ Src/routers/api.js
@@ -1,11 +1,30 @@
+// Importação do módulo "express"
 var express = require("express");
+
+// Criação de uma instância do Express
 var app = express();
+
+// Configuração do middleware para análise de dados codificados na URL
 app.use(express.urlencoded({ extended: true }));
+
+// Configuração do middleware para análise de dados no formato JSON
 app.use(express.json());
 
+// Criação do objeto de roteamento
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.status(200).send("<h1>API - CHAT</h1>");
-});
 
-app.use('/', router);
+// Definição da rota padrão ("/")
+app.use('/', router.get('/', (req, res) => {
+    res.status(200).send("<h1>API - CHAT</h1>");
+}));
+
+// Definição da rota "/sobre"
+app.use("/", router.get("/sobre", (req, res, next) => {
+    res.status(200).send({
+        "nome": "api-node-mongo",
+        "versão": "1.0.0",
+        "autor": "Gustavo Andrade"
+    });
+}));
+// Exportação do objeto "app" para uso em outros módulos
+module.exports = app;
--- bin/server.js
+++ bin/server.js
@@ -1,18 +1,22 @@
+// Importação do módulo "dotenv" para carregar variáveis de ambiente a partir de um arquivo .env
 require("dotenv").config();
-const express = require("express");
-const app = express();
 
+// Importação do módulo "app" que contém as rotas da API
+const app = require("../Src/routers/api");
+
+// Middleware para passar para o próximo middleware/rota
 app.use((req, res, next) => {
-  next();
+    next();
 });
 
+// Acessando a variável de ambiente API_PORT definida no arquivo .env
 console.log(process.env.API_PORT);
-const port = process.env.API_PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send("<h1>API - CHAT</h1>");
-});
+// Atribuição do valor da variável de ambiente API_PORT à variável "port"
+let port = process.env.API_PORT;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+// Inicialização do servidor para escutar na porta especificada
+app.listen(port);
+
+// Exibição do console informando em qual porta o servidor está ouvindo
+console.log(`listening on ${port}`);
