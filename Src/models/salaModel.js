4585fbb2dc744d70e57f0b491a93a263ba1fc532
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2023-05-24 19:02:36 -0300

    Pt 3 finalizada

--- Src/models/salaModel.js
+++ Src/models/salaModel.js
@@ -1,32 +1,33 @@
-module.exports = {listarSalas};
-function listarSalas(){
+const db = require("../models/modeldb");
+function listarSalas() {
+    return db.findAll("salas");
+}
+module.exports = { listarSalas };
+
+function listarSalas() {
     return [
         {
-            "id":{
+            "id": {
                 "$oid": "756463hhfh4858"
             },
-
-            "nome": "salamaleico ",
+            "nome": "salamaleico",
             "tipo": "publica"
         },
-        
         {
-            "id":{
+            "id": {
                 "$oid": "796809895804hfnm,"
             },
-
             "nome": "Cimolai só louco vai",
             "tipo": "privada",
             "chave": "at8q4haw"
         },
-
         {
-            "id":{
+            "id": {
                 "$oid": "3958930fjhe"
             },
-
             "nome": "Cimol hospício",
             "tipo": "publica"
         }
     ];
 }
+module.exports = { listarSalas };
